a3908f3f6adec5918938a7fbb6d2cd4d1587b658
LLITUPLEC <tminn91@mail.ru>
2020-11-25 16:42:31 +0400

    add M,V,C Chat

--- config/web.php
+++ config/web.php
@@ -5,6 +5,8 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'language'=>'ru',
+    'sourceLanguage'=>'ru',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
